d61e1153385bfbd1038ea1b5d9ab8dea7272a33e
Nadav Botzer <botzer14@gmail.com>
2025-01-08 12:48:04 +0200

    Fixing pathing

--- js/service/memeService.js
+++ js/service/memeService.js
@@ -1,26 +1,13 @@
 'use strict'
 var gImgs = [
-    { id: 1, url: './imgs/1.jpg', keywords: ['funny', 'cat'] },
-    { id: 2, url: './imgs/2.jpg', keywords: ['funny', 'cat'] }
+    { id: 1, url: '../../imgs/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: '../../imgs/2.jpg', keywords: ['funny', 'cat'] }
 ]
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
-    lines: [
-        {
-            id: 1,
-            txt: 'I sometimes eat Falafel',
-            size: 20,
-            color: 'red'
-        },
-        {
-            id: 2,
-            txt: 'and i like it',
-            size: 15,
-            color: 'red'
-        }
-    ],
-    linesCounter: 2
+    lines: [],
+    linesCounter: 0
 }
 
 
@@ -29,8 +16,7 @@ function getMeme() {
 }
 
 function setLineTxt(txt) {
-    console.log()
-    gMeme.lines[gMeme.selectedLineIdx].txt = txt
+    if (gMeme.lines.length) gMeme.lines[gMeme.selectedLineIdx].txt = txt
 }
 
 function getGallery() {
